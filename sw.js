7a164edc8d844146832a3b736882f0815627277c
Carmona <carmonafornesangel@gmail.com>
2026-09-23 00:26:54 +0200

    Agregando los primeros archivos

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'mi-dia-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Nunca cachear llamadas a Supabase: esos datos siempre deben ir a la red.
+  if (e.request.url.includes('supabase.co')) return;
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const network = fetch(e.request)
+        .then((res) => {
+          const clone = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
